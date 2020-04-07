5ef46a53ab6731f315c54f538c6f548a4ac4cc42
onadj <oliver.nad@gmail.com>
2020-04-07 19:40:21 +0000

    maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,23 +1,24 @@
+
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 4,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 58.162767,
+            lng: -5.216005
         }
     });
 
     var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
     var locations = [{
-        lat: 40.785091,
-        lng: -73.968285
+          lat: 58.162767,
+            lng: -5.216005
     }, {
-        lat: 41.084045,
-        lng: -73.874256
+           lat: 58.162767,
+            lng: -5.216005
     }, {
-        lat: 40.754932,
-        lng: -73.984016
+        lat: 58.162767,
+            lng: -5.216005
     }];
 
     var markers = locations.map(function (location, i) {
@@ -31,3 +32,4 @@ function initMap() {
         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
 }
+
