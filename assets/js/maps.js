5087733dd365a8cca5d3b097e15e8aaf5772c336
onadj <oliver.nad@gmail.com>
2020-04-09 13:43:45 +0000

    flags

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -3,6 +3,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: 58.162767,
             lng: -5.216005
@@ -23,50 +24,92 @@ function initMap() {
             lng: -5.216005
     }];
     
-
-       var markers = locations.map(function (location, i) {
+  var markers = locations.map(function (location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
+
 /*End Scotland*/
 
 
-
+/*Usa*/
 
     var map1 = new google.maps.Map(document.getElementById("map1"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: 37.799767,
             lng:  -122.434178
         }
     });
 
+ var labelsOne = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+
+    var locationsOne = [{
+        lat: 37.799767,
+            lng:  -122.434178
+    }, {
+           lat: 37.799767,
+            lng:  -122.434178
+    }, {
+        lat: 37.799767,
+            lng:  -122.434178
+    }];
+    
+ var markersOne = locationsOne.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            labelOne: labelsOne[i % labelsOne.length]
+        });
+    });
+ 
+
+
+/*EndUsa*/
+
+
+
+/*Australia*/
+
        var map2 = new google.maps.Map(document.getElementById("map2"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: -17.996430,
             lng:  145.794393
         }
     });
 
+
+ /*EndAustralia*/
+
+
+
+ /*Namibia*/
           var map3 = new google.maps.Map(document.getElementById("map3"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: -21.622705,
             lng:  14.224223
         }
     });
 
+/*EndNamibia*/
 
 
 
 
+/*EndNamibia*/
 
+
+/*Japan*/
             var map4 = new google.maps.Map(document.getElementById("map4"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: 35.359034, 
             lng: 138.726510
@@ -74,36 +117,56 @@ function initMap() {
     });
 
 
+/*EndJapan*/
+
+
+/*NewZealand*/
+
              var map5= new google.maps.Map(document.getElementById("map5"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: -38.950404,
             lng: 176.423715
         }
     });
 
+/*EndNewZealand*/
 
+
+
+
+/*Chile*/
              var map6= new google.maps.Map(document.getElementById("map6"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: -29.996407, 
             lng: -70.978098
         }
     });
+/*EndChile*/
 
 
-             var map7= new google.maps.Map(document.getElementById("map7"), {
+
+/*Canada*/
+       var map7= new google.maps.Map(document.getElementById("map7"), {
         zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
             lat: 52.812447, 
             lng: -117.894291
         }
     });
+/*EndCanada*/
 
 
+/*Markers*/
 
- 
+     
 
+
+/*EndMarkers*/
 
 
 
