d08680f2c31b47b138184a616cfef90086a4875d
onadj <oliver.nad@gmail.com>
2020-04-10 20:29:32 +0000

    maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,32 +1,137 @@
 
 /*Scotland*/
- function initMap() {
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRenderer = new google.maps.DirectionsRenderer();
-  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-  var mapOptions = {
-    zoom:7,
-    center: chicago
-  }
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  directionsRenderer.setMap(map);
-}
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: 58.162767,
+            lng: -5.216005
+        }
+    });
 
-function calcRoute() {
-  var start = document.getElementById('start').value;
-  var end = document.getElementById('end').value;
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: 'DRIVING'
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == 'OK') {
-      directionsRenderer.setDirections(result);
-    }
-  });
-}
 
+    
 
 /*End Scotland*/
 
+
+/*Usa*/
+
+    var map1 = new google.maps.Map(document.getElementById("map1"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: 37.799767,
+            lng:  -122.434178
+        }
+    });
+  
+ 
+
+/*EndUsa*/
+
+
+
+/*Australia*/
+
+       var map2 = new google.maps.Map(document.getElementById("map2"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: -17.996430,
+            lng:  145.794393
+        }
+    });
+
+
+ /*EndAustralia*/
+
+
+
+ /*Namibia*/
+          var map3 = new google.maps.Map(document.getElementById("map3"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: -21.622705,
+            lng:  14.224223
+        }
+    });
+
+/*EndNamibia*/
+
+
+
+
+/*EndNamibia*/
+
+
+/*Japan*/
+            var map4 = new google.maps.Map(document.getElementById("map4"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: 35.359034, 
+            lng: 138.726510
+        }
+    });
+
+
+/*EndJapan*/
+
+
+/*NewZealand*/
+
+             var map5= new google.maps.Map(document.getElementById("map5"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: -38.950404,
+            lng: 176.423715
+        }
+    });
+
+/*EndNewZealand*/
+
+
+
+
+/*Chile*/
+             var map6= new google.maps.Map(document.getElementById("map6"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: -29.996407, 
+            lng: -70.978098
+        }
+    });
+/*EndChile*/
+
+
+
+/*Canada*/
+       var map7= new google.maps.Map(document.getElementById("map7"), {
+        zoom: 5,
+        mapTypeId: google.maps.MapTypeId.HYBRID,
+        center: {
+            lat: 52.812447, 
+            lng: -117.894291
+        }
+    });
+/*EndCanada*/
+
+
+/*Markers*/
+
+      var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+
+}
+
+
+/*EndMarkers*/
+
+
+   
