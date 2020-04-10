55ffcb854e004802a727842fb584762ce272a7c2
onadj <oliver.nad@gmail.com>
2020-04-10 14:59:50 +0000

    destination

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,180 +1,38 @@
 
 /*Scotland*/
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: 58.162767,
-            lng: -5.216005
-        }
-    });
-
-
-    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-    var locations = [{
-          lat: 58.162767,
-            lng: -5.216005
-    }, {
-           lat: 58.162767,
-            lng: -5.216005
-    }, {
-        lat: 58.162767,
-            lng: -5.216005
-    }];
-    
-  var markers = locations.map(function (location, i) {
-      console.log("Making scotland markers")
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+ function initMap() {
+        var directionsRenderer = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: {lat: 37.77, lng: -122.447}
         });
-    });
+        directionsRenderer.setMap(map);
+
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+        document.getElementById('mode').addEventListener('change', function() {
+          calculateAndDisplayRoute(directionsService, directionsRenderer);
+        });
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+        var selectedMode = document.getElementById('mode').value;
+        directionsService.route({
+          origin: {lat: 37.77, lng: -122.447},  // Haight.
+          destination: {lat: 37.768, lng: -122.511},  // Ocean Beach.
+          // Note that Javascript allows us to access the constant
+          // using square brackets and a string value as its
+          // "property."
+          travelMode: google.maps.TravelMode[selectedMode]
+        }, function(response, status) {
+          if (status == 'OK') {
+            directionsRenderer.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
 
 
 /*End Scotland*/
-
-
-/*Usa*/
-  var labelsOne = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-    var map1 = new google.maps.Map(document.getElementById("map1"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: 37.799767,
-            lng:  -122.434178
-        }
-    });
-
- var labelsOne = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
-
-    var locationsOne = [{
-        lat: 37.799767,
-            lng:  -122.434178
-    }, {
-           lat: 37.799767,
-            lng:  -122.434178
-    }, {
-        lat: 37.799767,
-            lng:  -122.434178
-    }];
-    
- var markersOne = locationsOne.map(function (locationOne, i) {
-     console.log("Making usa markers")
-        return new google.maps.Marker({
-            position: locationOne,
-            labelOne: labelsOne[i % labelsOne.length]
-        });
-    });
- 
-
-
-/*EndUsa*/
-
-
-
-/*Australia*/
-
-       var map2 = new google.maps.Map(document.getElementById("map2"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: -17.996430,
-            lng:  145.794393
-        }
-    });
-
-
- /*EndAustralia*/
-
-
-
- /*Namibia*/
-          var map3 = new google.maps.Map(document.getElementById("map3"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: -21.622705,
-            lng:  14.224223
-        }
-    });
-
-/*EndNamibia*/
-
-
-
-
-/*EndNamibia*/
-
-
-/*Japan*/
-            var map4 = new google.maps.Map(document.getElementById("map4"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: 35.359034, 
-            lng: 138.726510
-        }
-    });
-
-
-/*EndJapan*/
-
-
-/*NewZealand*/
-
-             var map5= new google.maps.Map(document.getElementById("map5"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: -38.950404,
-            lng: 176.423715
-        }
-    });
-
-/*EndNewZealand*/
-
-
-
-
-/*Chile*/
-             var map6= new google.maps.Map(document.getElementById("map6"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: -29.996407, 
-            lng: -70.978098
-        }
-    });
-/*EndChile*/
-
-
-
-/*Canada*/
-       var map7= new google.maps.Map(document.getElementById("map7"), {
-        zoom: 5,
-        mapTypeId: google.maps.MapTypeId.HYBRID,
-        center: {
-            lat: 52.812447, 
-            lng: -117.894291
-        }
-    });
-/*EndCanada*/
-
-
-/*Markers*/
-
-     
-
-
-/*EndMarkers*/
-
-
-
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
-}
-
 
