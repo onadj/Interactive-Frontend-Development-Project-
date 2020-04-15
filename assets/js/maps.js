d38c2097a20fa626012928c64b24df4b53cdd8d4
onadj <oliver.nad@gmail.com>
2020-04-15 20:44:40 +0000

    maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,8 +1,6 @@
-
 function initMap() {
 
-      /*Scotland*/
-        var map = new google.maps.Map(document.getElementById('map'), {
+     var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 7,
           mapTypeId: google.maps.MapTypeId.HYBRID,
           center: {
@@ -11,16 +9,7 @@ function initMap() {
         }           
         });
 
-      
-
-
-       
-
-/*EndScotland*/
-
-       /*USA*/
-
-        var map1 = new google.maps.Map(document.getElementById("map1"), {
+ var map1 = new google.maps.Map(document.getElementById("map1"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -28,17 +17,8 @@ function initMap() {
             lng:  -122.434178
         }
     });
-  
 
- 
-
-/*EndUsa*/
-
-
-
-/*Australia*/
-
-       var map2 = new google.maps.Map(document.getElementById("map2"), {
+ var map2 = new google.maps.Map(document.getElementById("map2"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -47,13 +27,7 @@ function initMap() {
         }
     });
 
-
- /*EndAustralia*/
-
-
-
- /*Namibia*/
-          var map3 = new google.maps.Map(document.getElementById("map3"), {
+ var map3 = new google.maps.Map(document.getElementById("map3"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -62,16 +36,7 @@ function initMap() {
         }
     });
 
-/*EndNamibia*/
-
-
-
-
-/*EndNamibia*/
-
-
-/*Japan*/
-            var map4 = new google.maps.Map(document.getElementById("map4"), {
+var map4 = new google.maps.Map(document.getElementById("map4"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -80,13 +45,7 @@ function initMap() {
         }
     });
 
-
-/*EndJapan*/
-
-
-/*NewZealand*/
-
-             var map5= new google.maps.Map(document.getElementById("map5"), {
+   var map5= new google.maps.Map(document.getElementById("map5"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -95,13 +54,7 @@ function initMap() {
         }
     });
 
-/*EndNewZealand*/
-
-
-
-
-/*Chile*/
-             var map6= new google.maps.Map(document.getElementById("map6"), {
+ var map6= new google.maps.Map(document.getElementById("map6"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -109,12 +62,8 @@ function initMap() {
             lng: -70.978098
         }
     });
-/*EndChile*/
 
-
-
-/*Canada*/
-       var map7= new google.maps.Map(document.getElementById("map7"), {
+var map7= new google.maps.Map(document.getElementById("map7"), {
         zoom: 5,
         mapTypeId: google.maps.MapTypeId.HYBRID,
         center: {
@@ -122,9 +71,141 @@ function initMap() {
             lng: -117.894291
         }
     });
-/*EndCanada*/
-       
 
+///8 puta
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels3 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels5 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels6 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var labels7 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+
+    var locations = [
+        { lat:  57.478975, lng: -4.227523 }
+       
+    ];
+
+  var locations1 = [
+        { lat:  32.718007, lng: -117.152956}
+       
+    ];
+
+  var locations2 = [
+        { lat:  -33.865452, lng: 151.201777 }
+        
+    ];
+
+
+  var locations3 = [
+        { lat:  -22.213850, lng: 17.036723}
+        
+    ];
+
+
+  var locations4 = [
+        { lat:  36.155671, lng: 138.172384 }
+       
+    ];
+
+
+  var locations5 = [
+        { lat:  -42.427013, lng: 172.657816}
+        
+    ];
+
+
+
+  var locations6 = [
+        { lat:  -27.212575, lng: -70.385904 }
+       
+    ];
+
+
+
+  var locations7 = [
+        { lat:  56.352779, lng: -115.053676}
+        
+    ];
+
+
+
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers1 = locations1.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers2 = locations2.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers3 = locations3.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers4 = locations4.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers5 = locations5.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers6 = locations6.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+   var markers7 = locations7.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+ 
+
+ var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map1, markers1, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map2, markers2, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map3, markers3, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map4, markers4, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map5, markers5, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map6, markers6, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
+var markerCluster = new MarkerClusterer(map7, markers7, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
 }
-
